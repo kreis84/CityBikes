5d9bb05f3c31375592b6a6788c222a823c079df6
Krzysztof Mazurkiewicz <k3mazurkiewicz@gmail.com>
2017-02-16 12:19:16 +0100

    filtering by country

--- js/controllers.js
+++ js/controllers.js
@@ -4,12 +4,24 @@ angular.module('git')
 {
 	$scope.data = [];
 	$scope.countries = [];
+	$scope.byCountrySelect = false;
+	$scope.selectedCountry = '';
+	$scope.showCompanyList = '';
+
+	$scope.$watch('selectedCountry', function(){ 
+		if($scope.selectedCountry !== '') 
+			{
+				$scope.byCountrySelect = true;
+				$scope.showCompanyList = true;				
+			}
+	});
+
 	bikesFactory.bikes()
 	.then(
 		function(response)
 		{
 			$scope.data = response.data.networks;
-			countries = helperFactory.getCountries($scope.data));
+			$scope.countries = helperFactory.getCountries($scope.data);
 		},
 		function(error)
 		{
